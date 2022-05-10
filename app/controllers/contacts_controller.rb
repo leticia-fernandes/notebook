231dc4f7b726a0c4fa6e210a328ad368e3e61be9
Letícia Fernandes <leticiafs1994@hotmail.com>
2022-05-10 20:52:26 -0300

    Add CRUD for Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,51 @@
 class ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to @contact
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(contact_params)
+      redirect_to @contact
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :email)
+  end
 end
